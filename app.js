44a3e87fe510fea80bd4dd3593c174b07f5ea5a9
pa-tiq <patrickdsilva99@gmail.com>
2022-11-05 18:38:39 -0400

    styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const path = require ('path');
 const express = require('express');
+const rootDir = require('./util/path');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -6,12 +8,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded()); //parse the incoming request body
+app.use(express.static(path.join(rootDir,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => { 
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 }); 
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,11 @@
+const path = require ('path');
 const express = require('express');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/home',(req,res,next) => {
-  res.send('<h1>home</h1>');
+  //res.sendFile(path.join(__dirname,'..','views','shop.html'));
+  res.sendFile(path.join(rootDir,'views','shop.html'));
 }); 
 router.get('/',(req,res,next) => { 
   res.redirect('/home');
