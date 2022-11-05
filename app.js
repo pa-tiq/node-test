320819fd9e9d9f6b3160d07494d9efce3aa1f933
pa-tiq <patrickdsilva99@gmail.com>
2022-11-05 13:50:42 -0400

    express.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const http = require("http");
-const routes = require('./routes');
-const server = http.createServer(routes);
-server.listen(3000);
+const express = require('express');
+const app = express();
+app.use((req,res,next) => { //this function will be executed for every incoming request
+	console.log('in the middleware');
+  next();
+}); 
+app.use((req,res,next) => { //this function will be executed for every incoming request
+	console.log('in another middleware');
+  res.send('<h1>Oi</h1>');
+}); 
+app.listen(3000);
