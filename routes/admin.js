9937fe2721a927a17ad3b043c4273a3bd22f6ec0
Patrick Dantas <patrickdsilva99@gmail.com>
2022-11-07 17:33:49 -0400

    using pug as a template engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,19 @@
-const path = require ('path');
-const express = require('express');
-const rootDir = require('../util/path');
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product',(req,res,next) => {
+router.get("/add-product", (req, res, next) => {
   //res.sendFile(path.join(__dirname,'..','views','add-product.html'));
-  res.sendFile(path.join(rootDir,'views','add-product.html'));
+  //res.sendFile(path.join(rootDir,'views','add-product.html'));
+  res.render("add-product", { docTitle: "Add Product" });
 });
-router.post('/add-product',(req,res,next) => {
-  products.push({title: req.body.title});
-  res.redirect('/');
-}); 
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
 
 module.exports.routes = router;
 module.exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,14 @@
-const path = require ('path');
-const express = require('express');
-const rootDir = require('../util/path');
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
 
-const adminData = require('./admin');
+const adminData = require("./admin");
 
-router.get('/',(req,res,next) => {
-  console.log(adminData.products); //this data is shared with every user and every request
-  res.sendFile(path.join(rootDir,'views','shop.html'));
-}); 
+router.get("/", (req, res, next) => {
+  const products = adminData.products; //this data is shared with every user and every request
+  //res.sendFile(path.join(rootDir,'views','shop.html'));
+  res.render("shop", { docTitle: "Shop", products });
+});
 
 module.exports = router;
